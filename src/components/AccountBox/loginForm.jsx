9a8957ee4c68ab017787f36193cc3faf2206e35d
martinwangata <97232867+martinwangata@users.noreply.github.com>
2022-06-15 17:42:45 +0300

    mjengo smart

---
+++ src/components/AccountBox/loginForm.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+} from "./common";
+import { Marginer } from "../marginer";
+import { AccountContext } from "./accountContext";
+
+export function LoginForm(props) {
+  const { switchToSignup } = useContext(AccountContext);
+  const { switchToSignin } = useContext(AccountContext);
+
+  return (
+    <BoxContainer>
+      <FormContainer>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+      </FormContainer>
+      <Marginer direction="vertical" margin={10} />
+      <MutedLink href="#">Forget your password?</MutedLink>
+      <Marginer direction="vertical" margin="1.6em" />
+      <SubmitButton type="submit">Signin</SubmitButton>
+      <Marginer direction="vertical" margin="1em" />
+      <MutedLink href="#">
+        Don't have an account?{" "}
+        <BoldLink href="#" onClick={switchToSignup}>
+          Sign up Here
+        </BoldLink>
+      </MutedLink>
+      <MutedLink href="#">
+        Already Signed in?
+        <BoldLink href="#" onClick={switchToSignin}>
+          Go to Profile
+        </BoldLink>
+      </MutedLink>
+    </BoxContainer>
+  );
+}
